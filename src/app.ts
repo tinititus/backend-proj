f42e465f75bf371bb5af8a15e2612ae665d014f5
Tito <tito.carvalho@fdte.io>
2021-10-28 14:53:53 -0300

    add allowedMethods middleware

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,11 @@ import { router } from './routes'
 
 const app = new Koa()
 
+// app.on('error', (err, ctx): void => console.error('Server Error: ', err, ctx))
 app.use(logger())
 app.use(bodyParser())
+// todo: add cors, error handling, etc.
 app.use(router.routes())
+app.use(router.allowedMethods())
 
 export { app }
