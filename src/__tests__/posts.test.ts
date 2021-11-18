d42ea29ff119bc4445a044531c9ccd9779ac597e
tinititus <tito.carvalho@fdte.io>
2021-11-18 07:07:05 -0300

    update unit tests for PostService CRUD functionalities

--- src/__tests__/posts.test.ts
+++ src/__tests__/posts.test.ts
@@ -1,6 +1,12 @@
 import request from 'supertest'
 import { server } from '../server'
-import { CreatePostService, GetPostsService } from '../services/PostService'
+import {
+  CreatePostService,
+  DeletePostService,
+  GetPostByIdService,
+  GetPostsService,
+  UpdatePostService,
+} from '../services/PostService'
 import { prismaMock } from './../singleton'
 
 const newDate = new Date()
@@ -11,40 +17,50 @@ const post = {
   createdAt: newDate,
   updatedAt: newDate,
 }
+
 describe('POST /posts', () => {
   it('should create a new post', async () => {
     const service = new CreatePostService()
     prismaMock.post.create.mockResolvedValue(post)
-    await expect(service.execute(post.title, post.content)).resolves.toEqual({
-      id: '1',
-      title: 'title-test',
-      content: 'content-test',
-      createdAt: newDate,
-      updatedAt: newDate,
-    })
+    await expect(service.execute(post.title, post.content)).resolves.toEqual(
+      post
+    )
   })
 })
 
 describe('GET /posts', () => {
   it('should return all posts', async () => {
     const service = new GetPostsService()
-    prismaMock.post.findMany.mockResolvedValue([post, post])
-    await expect(service.execute()).resolves.toHaveLength(2)
+    prismaMock.post.findMany.mockResolvedValue([post, post, post])
+    await expect(service.execute()).resolves.toEqual([post, post, post])
   })
+})
 
-  // afterAll(() => {
-  //   return server.close()
-  // })
-  // it('should return all posts', async () => {
-  //     const res = await request(server).get('/posts')
-  //     expect(res.statusCode).toBe(200)
-  //     expect(res.body).toEqual(
-  //       expect.arrayContaining([
-  //         expect.objectContaining({
-  //           title: '1st post',
-  //           content: 'This is the first post'
-  //         }),
-  //       ])
-  //     )
-  //   })
+describe('GET /posts/:id', () => {
+  it('should return one post', async () => {
+    const service = new GetPostByIdService()
+    prismaMock.post.findUnique.mockResolvedValue(post)
+    await expect(service.execute(post.id)).resolves.toEqual(post)
+  })
+})
+
+describe('PATCH /posts/:id', () => {
+  it('should update post', async () => {
+    const service = new UpdatePostService()
+    prismaMock.post.update.mockResolvedValue({ ...post, title: 'new-title' })
+    await expect(service.execute(post.id, 'new-title')).resolves.toEqual({
+      ...post,
+      title: 'new-title',
+    })
+  })
+})
+
+describe('DELETE /posts/:id', () => {
+  it('should delete a post', async () => {
+    const service = new DeletePostService()
+    prismaMock.post.delete.mockResolvedValue(post)
+    await expect(service.execute(post.id)).resolves.toBe(
+      'Post (id: 1) deleted successfully'
+    )
+  })
 })
