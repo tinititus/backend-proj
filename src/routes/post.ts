59fec713c28764f38a770254ab26f920d2a79c7c
tinititus <tito.carvalho@fdte.io>
2021-11-26 00:16:12 -0300

    feat: check authentication when trying to change post data

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -6,13 +6,14 @@ import {
   GetPostsController,
   UpdatePostController,
 } from '../controllers/PostController'
+import { isAuth } from '../middlewares/isAuth'
 
 const router = new Router()
 
-router.post('/posts', new CreatePostController().handle)
-router.delete('/posts/:id', new DeletePostController().handle)
+router.post('/posts', isAuth, new CreatePostController().handle)
+router.delete('/posts/:id', isAuth, new DeletePostController().handle)
 router.get('/posts', new GetPostsController().handle)
 router.get('/posts/:id', new GetPostByIdController().handle)
-router.patch('/posts/:id', new UpdatePostController().handle)
+router.patch('/posts/:id', isAuth, new UpdatePostController().handle)
 
 export default router
