be3fe3a0fcd3d529980f6975027699c8e9e3ff7d
Tito <titoxmartini@gmail.com>
2022-01-25 23:28:52 -0300

    fix: import dotenv/config properly

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import { app } from './app'
-import * as dotenv from 'dotenv'
-
-dotenv.config()
+import 'dotenv/config'
 
 const server = app.listen(4000, () => {
   console.log('Server is running on port 4000')
